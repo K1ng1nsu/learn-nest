bab1b8e68b83562746b25e6373ec4f2de8c9e20c
K1ng1nsu <96197161+K1ng1nsu@users.noreply.github.com>
2025-01-23 13:31:27 +0900

    insu0123-7840-1

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -12,22 +12,24 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
-import { IsEmail } from 'class-validator';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private userSerive: UsersService) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
     return this.userSerive.create(body);
   }
 
+  // UseInterceptors(new SerializeInterceptor(UserDto));
+  // @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id', ParseIntPipe) id: string) {
-    console.log(id);
     const user = await this.userSerive.findOne(+id);
     if (!user) throw new NotFoundException('해당 유저를 찾을 수 없음');
 
